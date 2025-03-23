0a346a4873a6923e91ed10d998f67e7ac652bbbf
yarenarslan <yarenarslan0997@gmail.com>
2025-03-23 11:37:27 +0300

    Fix import name from users to user_routes

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
-from app.routes import users
+from app.routes import user_routes
 
 router = APIRouter()
 
 # Kullanıcı rotalarını ana router'a ekleyelim
-router.include_router(users.router, prefix="/users", tags=["Users"])
+router.include_router(user_routes.router, prefix="/users", tags=["Users"])
